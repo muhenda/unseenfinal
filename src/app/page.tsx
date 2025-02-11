0b7fff88773308964a648bf21028c2e011b4316f
muhenda <robpro888@gmail.com>
2025-02-11 12:27:42 +0300

    Add missing files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,23 @@
 import Base from "./pages/Page";
 
-
 export default function Home() {
   return (
     <>
-    <Base />
+      {/* Link to Stylesheets */}
+      <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+      <link rel="stylesheet" href="assets/css/owl.carousel.min.css" />
+      <link rel="stylesheet" href="assets/css/animate.css" />
+      <link rel="stylesheet" href="assets/css/magnific-popup.css" />
+      <link rel="stylesheet" href="assets/css/all.min.css" />
+      <link rel="stylesheet" href="assets/css/flaticon.css" />
+      <link rel="stylesheet" href="assets/css/font.css" />
+      <link rel="stylesheet" href="assets/css/metisMenu.css" />
+      <link rel="stylesheet" href="assets/css/nice-select.css" />
+      <link rel="stylesheet" href="assets/css/slick.css" />
+      <link rel="stylesheet" href="assets/css/spacing.css" />
+      <link rel="stylesheet" href="assets/css/main.css" />
+
+      <Base />
     </>
   );
 }
