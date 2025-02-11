7bc3c9906222cfb3a067faadba7073555551f2c5
muhenda <robpro888@gmail.com>
2025-02-11 14:13:38 +0300

    Add missing files

--- src/app/layout copy.tsx
+++ src/app/layout copy.tsx
@@ -35,18 +35,18 @@ export default function RootLayout({
         <link rel="apple-touch-icon" href="/logo192.png" />
         
         {/* External Stylesheets */}
-        <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
-        <link rel="stylesheet" href="assets/css/owl.carousel.min.css" />
-        <link rel="stylesheet" href="assets/css/animate.css" />
-        <link rel="stylesheet" href="assets/css/magnific-popup.css" />
-        <link rel="stylesheet" href="assets/css/all.min.css" />
-        <link rel="stylesheet" href="assets/css/flaticon.css" />
-        <link rel="stylesheet" href="assets/css/font.css" />
-        <link rel="stylesheet" href="assets/css/metisMenu.css" />
-        <link rel="stylesheet" href="assets/css/nice-select.css" />
-        <link rel="stylesheet" href="assets/css/slick.css" />
-        <link rel="stylesheet" href="assets/css/spacing.css" />
-        <link rel="stylesheet" href="assets/css/main.css" />
+        <link rel="stylesheet" href="https://unseenmodelagency.netlify.app/assets/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="https://unseenmodelagency.netlify.app/assets/css/owl.carousel.min.css" />
+        <link rel="stylesheet" href="https://unseenmodelagency.netlify.app/assets/css/animate.css" />
+        <link rel="stylesheet" href="https://unseenmodelagency.netlify.app/assets/css/magnific-popup.css" />
+        <link rel="stylesheet" href="https://unseenmodelagency.netlify.app/assets/css/all.min.css" />
+        <link rel="stylesheet" href="https://unseenmodelagency.netlify.app/assets/css/flaticon.css" />
+        <link rel="stylesheet" href="https://unseenmodelagency.netlify.app/assets/css/font.css" />
+        <link rel="stylesheet" href="https://unseenmodelagency.netlify.app/assets/css/metisMenu.css" />
+        <link rel="stylesheet" href="https://unseenmodelagency.netlify.app/assets/css/nice-select.css" />
+        <link rel="stylesheet" href="https://unseenmodelagency.netlify.app/assets/css/slick.css" />
+        <link rel="stylesheet" href="https://unseenmodelagency.netlify.app/assets/css/spacing.css" />
+        <link rel="stylesheet" href="https://unseenmodelagency.netlify.app/assets/css/main.css" />
         
         {/* Favicon */}
         <link rel="shortcut icon" href="assets/img/favicon.png" type="image/x-icon" />
@@ -56,25 +56,25 @@ export default function RootLayout({
         {children}
 
         {/* Adding scripts using Next.js Script component for better handling */}
-        <Script src="assets/js/vendor/modernizr-3.5.0.min.js" strategy="beforeInteractive" />
-        <Script src="assets/js/vendor/jquery-2.2.4.min.js" strategy="beforeInteractive" />
-        <Script src="assets/js/popper.min.js" strategy="beforeInteractive" />
-        <Script src="assets/js/bootstrap.min.js" strategy="beforeInteractive" />
-        <Script src="assets/js/owl.carousel.min.js" strategy="beforeInteractive" />
-        <Script src="assets/js/isotope.pkgd.min.js" strategy="beforeInteractive" />
-        <Script src="assets/js/slick.min.js" strategy="beforeInteractive" />
+        <Script src="https://unseenmodelagency.netlify.app/assets/js/vendor/modernizr-3.5.0.min.js" strategy="beforeInteractive" />
+        <Script src="https://unseenmodelagency.netlify.app/assets/js/vendor/jquery-2.2.4.min.js" strategy="beforeInteractive" />
+        <Script src="https://unseenmodelagency.netlify.app/assets/js/popper.min.js" strategy="beforeInteractive" />
+        <Script src="https://unseenmodelagency.netlify.app/assets/js/bootstrap.min.js" strategy="beforeInteractive" />
+        <Script src="https://unseenmodelagency.netlify.app/assets/js/owl.carousel.min.js" strategy="beforeInteractive" />
+        <Script src="https://unseenmodelagency.netlify.app/assets/js/isotope.pkgd.min.js" strategy="beforeInteractive" />
+        <Script src="https://unseenmodelagency.netlify.app/assets/js/slick.min.js" strategy="beforeInteractive" />
         <Script src="assets/js/metisMenu.min.js" strategy="beforeInteractive" />
-        <Script src="assets/js/jquery.nice-select.js" strategy="beforeInteractive" />
-        <Script src="assets/js/ajax-form.js" strategy="beforeInteractive" />
-        <Script src="assets/js/wow.min.js" strategy="beforeInteractive" />
-        <Script src="assets/js/jquery.counterup.min.js" strategy="beforeInteractive" />
-        <Script src="assets/js/waypoints.min.js" strategy="beforeInteractive" />
-        <Script src="assets/js/jquery.scrollUp.min.js" strategy="beforeInteractive" />
-        <Script src="assets/js/imagesloaded.pkgd.min.js" strategy="beforeInteractive" />
-        <Script src="assets/js/jquery.magnific-popup.min.js" strategy="beforeInteractive" />
-        <Script src="assets/js/jquery.easypiechart.js" strategy="beforeInteractive" />
-        <Script src="assets/js/plugins.js" strategy="beforeInteractive" />
-        <Script src="assets/js/main.js" strategy="beforeInteractive" />
+        <Script src="https://unseenmodelagency.netlify.app/assets/js/jquery.nice-select.js" strategy="beforeInteractive" />
+        <Script src="https://unseenmodelagency.netlify.app/assets/js/ajax-form.js" strategy="beforeInteractive" />
+        <Script src="https://unseenmodelagency.netlify.app/assets/js/wow.min.js" strategy="beforeInteractive" />
+        <Script src="https://unseenmodelagency.netlify.app/assets/js/jquery.counterup.min.js" strategy="beforeInteractive" />
+        <Script src="https://unseenmodelagency.netlify.app/assets/js/waypoints.min.js" strategy="beforeInteractive" />
+        <Script src="https://unseenmodelagency.netlify.app/assets/js/jquery.scrollUp.min.js" strategy="beforeInteractive" />
+        <Script src="https://unseenmodelagency.netlify.app/assets/js/imagesloaded.pkgd.min.js" strategy="beforeInteractive" />
+        <Script src="https://unseenmodelagency.netlify.app/assets/js/jquery.magnific-popup.min.js" strategy="beforeInteractive" />
+        <Script src="https://unseenmodelagency.netlify.app/assets/js/jquery.easypiechart.js" strategy="beforeInteractive" />
+        <Script src="https://unseenmodelagency.netlify.app/assets/js/plugins.js" strategy="beforeInteractive" />
+        <Script src="https://unseenmodelagency.netlify.app/assets/js/main.js" strategy="beforeInteractive" />
       </body>
     </html>
   );
