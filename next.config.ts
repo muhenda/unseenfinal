7d09795cb70eddbcdbc36020ef8a6b7d197b3363
muhenda <robpro888@gmail.com>
2025-02-11 17:06:42 +0300

    Add missing files

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from 'next';
+import type { Configuration } from 'webpack';
+
+const nextConfig: NextConfig = {
   images: {
-    domains: ['firebasestorage.googleapis.com'], // Add Firebase Storage domain
+    domains: ['firebasestorage.googleapis.com'],
+  },
+  webpack: (config: Configuration) => {
+    config.module?.rules?.push({ // Use optional chaining and nullish coalescing
+      test: /\.js$/,
+      exclude: /(src\/app\/assets\/js|node_modules)/,
+    });
+    return config;
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
